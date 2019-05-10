48f7324751f0c303a3c2e0336c4536a19cac8e31
tp <tp@octavore.com>
2019-05-10 10:48:24 -0700

    Fix PAC file to use configured port.

--- pac.go
+++ pac.go
@@ -1,11 +1,19 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"net/url"
+)
 
 func (a *App) pacFile(rw http.ResponseWriter, req *http.Request) {
+	u, err := url.Parse(a.config.Addr)
+	if err != nil {
+		log.Fatalln("error parsing addr in config file:", err)
+	}
 	rw.Write([]byte(`function FindProxyForURL(url, host) {
 	if (shExpMatch(host, "*.` + a.config.TLD + `")) {
-		return "PROXY 127.0.0.1:7999";
+		return "PROXY 127.0.0.1:` + u.Port() + `";
 	}
 	return "DIRECT";
 }
