c39265920976e58b50cfd96e716d24f2b9fb5c45
tp <tp@octavore.com>
2019-05-15 17:59:48 -0700

    Tweak to use dnsDomainIs instead of shExpMatch in pac file.

--- pac.go
+++ pac.go
@@ -9,7 +9,7 @@ func (a *App) pacFile(rw http.ResponseWriter, req *http.Request) {
 	parts := strings.Split(a.config.Addr, ":")
 	port := parts[len(parts)-1]
 	rw.Write([]byte(`function FindProxyForURL(url, host) {
-	if (shExpMatch(host, "*.` + a.config.TLD + `")) {
+	if (dnsDomainIs(host, ".` + a.config.TLD + `")) {
 		return "PROXY 127.0.0.1:` + port + `";
 	}
 	return "DIRECT";
